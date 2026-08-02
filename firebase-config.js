1ec79a91b89bb3184e8152b6b9b54828842ebacb
AMAL Al-Guwaizani <37028672+amalabdulaziz@users.noreply.github.com>
2026-08-02 20:17:19 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCRerGp0Wfj7QoxzMNYnWX0zmtg1NAEBls",
+  authDomain: "letters-game-ff80c.firebaseapp.com",
+  databaseURL: "https://letters-game-ff80c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "letters-game-ff80c",
+  storageBucket: "letters-game-ff80c.firebasestorage.app",
+  messagingSenderId: "685369665098",
+  appId: "1:685369665098:web:39620f12bd8a797a06a8b3"
+};
